62a333ecb17a96c63b554df09e600567acec2e11
Marian Marian <fenrir55@gmail.com>
2021-07-01 21:08:51 +0200

    Add project files.

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var employeeRepository = new GenericRepositoryRemove<Employee, int>();
+            employeeRepository.Key = 1;
+            employeeRepository.Add(new Employee { FirstName = "Robert" });
+            employeeRepository.Add(new Employee { FirstName = "Staszek" });
+            employeeRepository.Add(new Employee { FirstName = "Tomek" });
+            employeeRepository.Save();
+
+            var orgRepository = new GenericRepository<Organization, Guid>();
+            orgRepository.Key = new Guid();
+            orgRepository.Add(new Organization { Name = "RobertCompany" });
+            orgRepository.Add(new Organization { Name = "StaszekCompany" });
+            orgRepository.Save();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WiredBrainCoffee.StorageApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public class GenericRepository<T, TKey>
+    {
+        protected readonly List<T> _items = new();
+        public TKey? Key { get; set; }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Save()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class GenericRepositoryRemove<T, TKey> : GenericRepository<T, TKey>
+    {
+        public void Remove(T item)
+        {
+            _items.Remove(item);
+        }
+    }
+}
